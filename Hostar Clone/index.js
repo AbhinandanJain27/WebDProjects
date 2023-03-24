92c206f6e5d1f27fd145f8afba8c8db050fca21c
Abhinandan <coolaj2712@gmail.com>
2023-03-24 15:30:14 +0530

    upadtes done

--- Hostar Clone/index.js
+++ Hostar Clone/index.js
@@ -1,49 +1,111 @@
- let movies = [
- {
-    name: "Falcon and the winter soldier",
-    des : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
-    image: "images/slider2.png"
- },
- {
-    name: "Loki",
-    des : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
-    image: "images/slider1.png"
- },
- {
-    name: "Wanda Vision",
-    des : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
-    image: "images/slider3.png"
- },
- {
-    name: "raya and the last dragon",
-    des : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
-    image: "images/slider4.png"
- },
- {
-    name: "luca",
-    des : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
-    image: "images/slider5.png"
- }
+let movies = [
+   {
+      name: "Falcon and the winter soldier",
+      des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
+      image: "images/slider 2.png"
+   },
+   {
+      name: "Loki",
+      des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
+      image: "images/slider 1.PNG"
+   },
+   {
+      name: "Wanda Vision",
+      des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
+      image: "images/slider 3.png"
+   },
+   {
+      name: "raya and the last dragon",
+      des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
+      image: "images/slider 4.png"
+   },
+   {
+      name: "luca",
+      des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ratione quisquam quibusdam voluptatum doloribus repudiandae.",
+      image: "images/slider 5.png"
+   }
 ];
 
 const carousel = document.querySelector('.carousel');
-let sliders=[];
+let sliders = [];
 
 let slideIndex = 0;//track the current slide
 
 const createslide = () => {
-    if (slideindex>movies.length)
-        slideindex=0;
+   if (slideIndex >= movies.length) {
+      slideIndex = 0;
+   }
+
+   // create dom element
+   let slide = document.createElement('div');
+   var imgElement = document.createElement('img');
+   let content = document.createElement('div');
+   let h1 = document.createElement('h1');
+   let p = document.createElement('p');
+
+   //Attaching All Elements
+   imgElement.appendChild(document.createTextNode(''));
+   h1.appendChild(document.createTextNode(movies[slideIndex].name));
+   p.appendChild(document.createTextNode(movies[slideIndex].des));
+   content.appendChild(h1);
+   content.appendChild(p);
+   slide.appendChild(imgElement);
+   carousel.appendChild(slide);
+
+   // setting up images
+   imgElement.src = movies[slideIndex].image;
+   slideIndex++;
+
+   // setting elements classname
+   slide.className = 'slider';
+   content.className = 'slide-content';
+   h1.className = 'movie-title';
+   p.className = 'movie-des';
+
+   sliders.push(slide);
+
+   if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% -${30 * (sliders.length - 2)}px)`;
+   }
+};
+
+for (let i=0; i<3; i++) {
+   createslide();
 }
 
-// create dom element
-let slide = document.createElement('div');
-var imgElement = document.createElement('img');
-let content = document.createElement('div');
-let h1 = document.createElement('h1');
-let p = document.createElement('p');
+setInterval(() => {
+   createslide();
+} , 3000);
 
-//Attaching All Elements
-imgElement.appendChild(document.createTextNode(''));
-h1.appendChild(document.createTextNode(movies[slideindex].name));
-p.appendChild(document.createTextNode(movies[slideIndex].des));
+//video Cards
+const videoCards=[...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item=>{
+   item.addEventListener('mouseover',()=>{
+      let video = item.children[1];
+      video.play();
+   });
+   item.addEventListener('mouseleave',()=>{
+      let video = item.children[1];
+      video.pause();
+   });
+});
+
+//card slider
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((items,i)=>{
+   let containerDimensions = items.getBoundingClientRect();
+   let containerWidth= containerDimensions.width;
+
+   nxtBtns[i].addEventListener('click',()=>{
+      items.scrollLeft += containerWidth - 200;
+   })
+
+   preBtns[i].addEventListener('click',()=>{
+      items.scrollLeft -= containerWidth + 200;
+   })
+})
